08f6504df8b232849cfc1fc019c4f2a573e90aff
jaeeung0601 <choijaee9@gmail.com>
2022-09-02 21:04:54 +0900

    강사님

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 import '../App.css';
-import React,{useState} from 'react';
-import Router from './Router'
+import React,{useEffect, useState} from 'react';
+import Router from 'components/Router'
+import {authService} from 'fbase'
 
 function App() {
+  const [init,setInit] = useState(false)
+  const [isLoggedIn,setIsLoggedIn] = useState(false)
+  useEffect(()=>{
+    authService.onAuthStateChanged((user)=>{
+      if(user){
+        setIsLoggedIn(true)
+      }else{
+        setIsLoggedIn(false)
+      }
+      setInit(true)
+      })
+    },[])
   return (
     <div>
-      <Router />
+      {init ? <Router isLoggedIn={isLoggedIn} /> : 'Initializing...' }
+      <footer>&copy; Nwitter {new Date().getFullYear()} </footer>
     </div>
   );
 }
